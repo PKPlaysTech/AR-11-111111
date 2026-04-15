0aa173ae17edf0cb3eaff798282d78fc3fc4068e
peileeleepei-eng <peilee.leepei@gmail.com>
2026-04-15 16:09:31 +0800

    Fix white screen error

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import TeacherDashboard from "./pages/teacher/TeacherDashboard";
 import GameEditor from "./pages/teacher/GameEditor";
